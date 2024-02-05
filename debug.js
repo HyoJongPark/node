cd6b8cb488e4e3f1099193d0535592e0e2c399d4
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-02-05 16:07:28 +0900

    feat: 기본 테스트 파일 세팅

---
+++ debug.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+  console.log('setTimeout');
+  setImmediate(() => {
+    console.log('setImmediate 2');
+  });
+}, 10);
+
+setImmediate(() => {
+  console.log('setImmediate 1');
+});
